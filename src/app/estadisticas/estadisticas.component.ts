3c324a2068cfa5653602436aec2e730b09dcbc72
Jesus <jjesusalexpuente@gmail.com>
2020-04-02 23:51:59 +0200

    tarea(*): Incluido añadir y eliminar pestañas

--- src/app/estadisticas/estadisticas.component.ts
+++ src/app/estadisticas/estadisticas.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl} from '@angular/forms';
+
 
 interface Variables {
   value: string;
@@ -11,11 +13,24 @@ interface Variables {
   styleUrls: ['./estadisticas.component.css']
 })
 export class EstadisticasComponent implements OnInit {
-  variables: String[] = ['Velocidad', 'Potencia', 'Aceleracion', 'Desviacion', 'Intensidad Solar', 'Direccion del Viento', 'Intensidad del Viento']
-  tabs = ['Una', 'Dos', 'Tres'];
+  variables: String[] = ['Velocidad', 'Potencia', 'Aceleracion', 'Desviacion', 'Intensidad Solar', 'Direccion del Viento', 'Intensidad del Viento', 'Tiempo']
+  tabs = ['Velocidad - Potencia', 'Aceleracion - Desviacion', 'Intensidad Solar - Tiempo'];
+  selected = new FormControl(0);
+  cx: String = ""
+  cy: String = ""
   constructor() { }
 
   ngOnInit(): void {
+  }
+  addTab(cx:String, cy:String){
+    if(cx !== "" && cy !=="" && cx!==cy){
+    this.tabs.push(cx + ' - ' + cy);
+    this.selected.setValue(this.tabs.length - 1);
+    }
+  }
+
+  removeTab(index: number) {
+    this.tabs.splice(index, 1);
   }
 
 }
