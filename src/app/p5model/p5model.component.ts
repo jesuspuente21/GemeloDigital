dfb7f7a8a3abfdf28ac640dd639e548aa933a1f8
Jesus <jjesusalexpuente@gmail.com>
2020-04-08 19:43:57 +0200

    tarea(*): Incluido rozamiento mas trayectoria del barco

--- src/app/p5model/p5model.component.ts
+++ src/app/p5model/p5model.component.ts
@@ -27,6 +27,27 @@ export class P5modelComponent implements AfterViewInit {
       var movers
       var grid
       var force
+      var wind
+      var bounds = document.getElementById('sketch-holder').getBoundingClientRect();
+      var Drag = function(c, turbulence) {
+        this.c = c;
+        this.turbulence = turbulence;
+    };
+
+    Drag.prototype.calculateDrag = function(m) {
+      // Magnitude is coefficient * speed squared
+      var speed = m.velocity.mag();
+      var dragMagnitude = this.c * s.pow(speed, this.turbulence);
+      
+      // Direction is inverse of velocity
+      var dragForce = m.velocity.clone();
+      dragForce.mult(-1);
+      
+      // Scale according to magnitude
+      dragForce.normalize();
+      dragForce.mult(dragMagnitude);
+      return dragForce;
+  }
 
       var Grid = function (x, y, xscale, yscale) {
         this.xorigin = 0;
@@ -54,25 +75,26 @@ export class P5modelComponent implements AfterViewInit {
 
       Grid.prototype.display = function (dirx, diry) {
         if (dirx && diry) {
-          for (var x = this.xorigin; x < this.realwidth; x += this.scale.x) {
+          for (var x = this.realwidth; x > this.xorigin; x -= this.scale.x) {
             for (var y = this.realheight; y > this.yorigin; y -= this.scale.y) {
               this.printLines(x, y)
             }
           }
         } else if (dirx && !diry) {
-          for (var x = this.xorigin; x < this.realwidth; x += this.scale.x) {
+          for (var x = this.realwidth; x > this.xorigin; x -= this.scale.x) {
             for (var y = this.yorigin; y < this.realheight; y += this.scale.y) {
               this.printLines(x, y)
             }
           }
         } else if (!dirx && diry) {
-          for (var x = this.realwidth; x > this.xorigin; x -= this.scale.x) {
+
+          for (var x = this.xorigin; x < this.realwidth; x += this.scale.x) {
             for (var y = this.realheight; y > this.yorigin; y -= this.scale.y) {
               this.printLines(x, y)
             }
           }
         } else {
-          for (var x = this.realwidth; x > this.xorigin; x -= this.scale.x) {
+          for (var x = this.xorigin; x < this.realwidth; x += this.scale.x) {
             for (var y = this.yorigin; y < this.realheight; y += this.scale.y) {
               this.printLines(x, y)
             }
@@ -85,6 +107,8 @@ export class P5modelComponent implements AfterViewInit {
         this.position = new PVector(x, y);
         this.velocity = new PVector(0, 0);
         this.acceleration = new PVector(0, 0);
+        this.xvals = []
+        this.yvals = []
       };
 
       Mover.prototype.applyForce = function (force) {
@@ -94,34 +118,46 @@ export class P5modelComponent implements AfterViewInit {
 
       Mover.prototype.update = function () {
         this.velocity.add(this.acceleration);
-        if (this.position.y + this.velocity.y / grid.scale.y > (s.height - s.height / 10) || this.position.y + this.velocity.y / grid.scale.y < (s.height / 10)) grid.update(0, this.velocity.y / grid.scale.y);
-        else this.position.y += this.velocity.y / grid.scale.y
-        if (this.position.x + this.velocity.x / grid.scale.x > (s.width - s.width / 10) || this.position.x + this.velocity.x / grid.scale.x < (s.width / 10)) grid.update(this.velocity.x / grid.scale.x, 0);
-        else this.position.x += this.velocity.x / grid.scale.x
+        if (this.position.y + this.velocity.y >= (s.height - s.height / 10) || this.position.y + this.velocity.y< (s.height / 10)) grid.update(0, this.velocity.y);
+        this.position.y += this.velocity.y
+        if (this.position.x + this.velocity.x >= (s.width - s.width / 10) || this.position.x + this.velocity.x < (s.width / 10)) grid.update(this.velocity.x, 0);
+        this.position.x += this.velocity.x
         this.acceleration.mult(0);
+        this.xvals.push((this.position.x))
+        this.yvals.push((this.position.y))
       };
 
       Mover.prototype.display = function () {
         s.stroke(sc);
         s.strokeWeight(sw);
         s.fill(123, 217, 176);
-        s.ellipse(this.position.x, this.position.y, this.mass * 16, this.mass * 16);
+        var boatx = s.map(this.position.x, grid.xorigin, grid.realwidth, 0 , bounds.width)
+        var boaty = s.map(this.position.y, grid.yorigin, grid.realheight, 0 , bounds.height)
+        s.triangle(boatx - this.mass *2000/  (grid.realwidth - grid.xorigin), boaty, boatx + this.mass  *2000/  (grid.realwidth - grid.xorigin), boaty, boatx, boaty - 2 * this.mass  *3000/  (grid.realheight - grid.yorigin));
+        for (let i = 0; i < this.xvals.length; i++) {
+          let px = s.map(this.xvals[i],  grid.xorigin  , grid.realwidth, 0, bounds.width) 
+          let py = s.map(this.yvals[i], grid.yorigin, grid.realheight , 0, bounds.height) 
+          s.stroke('#F9A825')
+          s.point(px, py);
+        }
       };
 
       var reset = function () {
-        var bounds = document.getElementById('sketch-holder').getBoundingClientRect();
-        movers = new Mover(s.random(0.5, 3), s.width / 2, s.height - s.height / 8);
+        
+        movers = new Mover(2, s.width / 2, s.height - s.height / 8);
         grid = new Grid(bounds.width, bounds.height, bounds.width / 15, bounds.height / 15)
-        force = new PVector(0.00 * movers.mass, -0.9 * movers.mass);
+        force = new PVector(0.00 * movers.mass, -0.03 * movers.mass);
+        wind = new PVector(0.08 * movers.mass, 0 * movers.mass);
       }
 
       s.setup = () => {
-        var bounds = document.getElementById('sketch-holder').getBoundingClientRect();
         let canvas = s.createCanvas(bounds.width, bounds.height);
         canvas.parent('sketch-holder');
         s.rect(0, 0, s.width, s.height);
         reset();
       };
+
+      var drag = new Drag(0.02, 1.5);
 
       s.draw = () => {
 
@@ -131,12 +167,15 @@ export class P5modelComponent implements AfterViewInit {
           console.log(force.y + "-----------------------")
           force.y = force.y * -1
         }*/
+        var dragForce = drag.calculateDrag(movers);
+        movers.applyForce(dragForce);
         // Apply gravity
         movers.applyForce(force);
+        movers.applyForce(wind);
 
         // Update and display
         movers.update();
-        grid.display(movers.velocity.x > 0, movers.velocity.y < 0)
+        grid.display(movers.velocity.x < 0, movers.velocity.y < 0)
         movers.display();
         console.log(movers.velocity.y)
       };
